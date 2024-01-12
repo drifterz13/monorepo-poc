ba8855c029b729d08042218403e1f8f2978e5baf
drifterz13 <totsawat@skilllane.com>
2024-01-12 13:24:13 +0700

    ci: trigger ci

---
+++ scripts/docker_build_and_push.mjs
@@ -0,0 +1,20 @@
+#!/usr/bin/env zx
+
+const SERVICES = ["school-web", "school-core-api"];
+const DOCKER_USERNAME = "sklmix"; // TODO: Change this to GCP docker username.
+const ENVIRONMENT = "dev";
+
+const PUBLISHED_PACKAGES = JSON.parse(argv.packages);
+
+const TARGET_SERVICES = PUBLISHED_PACKAGES.filter((pkg) =>
+  SERVICES.includes(pkg.name)
+);
+
+for (const SERVICE of TARGET_SERVICES) {
+  const SERVICE_NAME = SERVICE.name;
+  const SERVICE_VERSION = SERVICE.version;
+
+  await $`docker build . --target ${SERVICE_NAME} --tag ${SERVICE_NAME}:${SERVICE_VERSION}`;
+  await $`docker tag ${SERVICE_NAME}:${SERVICE_VERSION} ${DOCKER_USERNAME}/${SERVICE_NAME}:${SERVICE_VERSION}-${ENVIRONMENT}`;
+  await $`docker push ${DOCKER_USERNAME}/${SERVICE_NAME}:${SERVICE_VERSION}-${ENVIRONMENT}`;
+}
